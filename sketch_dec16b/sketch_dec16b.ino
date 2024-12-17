33891232f8176e51b5b9467ec19098561d62f532
flash_user <hw@yuanhau.com>
2024-12-17 14:21:12 +0800

    Add LED control and enhance event handling in sketch_dec16b

--- sketch_dec16b/sketch_dec16b.ino
+++ sketch_dec16b/sketch_dec16b.ino
@@ -3,9 +3,13 @@
 
 BMS56M605 mpu(8);
 
+
 void setup() {
   mpu.begin();
   Serial.begin(9600);
+  Serial.println("a \n");
+  pinMode(LED_BUILTIN, OUTPUT);
+
 }
 
 void loop() {
@@ -23,21 +27,28 @@ void event() {
     //}
     if (gx < -100 && gy < -100) {
         Serial.println("Oops! You just fell down on the floor! -x & -y");
+        
     }
     if (gx > 100 && gy > 100) {
         Serial.println("Oops! You just fell down on the floor! x & y");
+        btn1();
     }
     if (gx < -100 && gz < -100) {
         Serial.println("Oops! You just fell down on the floor! -x & -z");
+        btn1();
+
     }
     if (gx > 100 && gz > 100) {
         Serial.println("Oops! You just fell down on the floor! x & z");
+        btn1();
     }
     if (gz < -100 && gy < -100) {
         Serial.println("Oops! You just fell down on the floor! -y & -z");
+        btn1();
     }
     if (gy > 100 && gz > 100) {
         Serial.println("Oops! You just fell down on the floor! y & z");
+        btn1();
     }
     //if (gy < -100) {
     //    Serial.println("Oops! You just fell down on the floor! y");
@@ -46,4 +57,10 @@ void event() {
     //    Serial.println("Oops! You just fell down on the floor! z");
     //}
     delay(100);
+}
+void btn1() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(10);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(10);  
 }
