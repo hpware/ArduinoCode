743b53643e3deb97e6daa344310ded248ee0ba72
flash_user <hw@yuanhau.com>
2025-02-24 08:23:46 +0800

    Refactor pin assignments and add HTTP server functionality with sensor readings

---
+++ sketch_feb22a/sketch_feb22a.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Init");
+}
+
+void loop() {
+
+}
--- sketch_feb22c/sketch_feb22c.ino
+++ sketch_feb22c/sketch_feb22c.ino
@@ -1,19 +1,27 @@
-const int beeppin = 3;
-const int balsepin = 9;
+const int beeppin = 0;
+const int balsepin = 1;
+const int balsepind = 2;
 
 void setup() {
-  pinMode(balsepin, INPUT);
-  Serial.begin(115200);
+  Serial.begin(460800);
   pinMode(beeppin, OUTPUT);
+  pinMode(balsepin, INPUT);
+  pinMode(balsepind, INPUT);
   beep(50); 
   beep(50);
   delay(1000);
 }
 
 void loop() {
-  int sensorState = digitalRead(balsepin);
-  Serial.println(sensorState);
-  delay(100);
+  int sensorState = analogRead(balsepin);
+  int sensorStated = digitalRead(balsepind);
+  Serial.print("Analog: ");
+  Serial.print(sensorState);
+  Serial.print("  Digital: ");
+  Serial.println(sensorStated);
+  if (sensorStated) {
+    beep(100);
+  }
 }
 void beep(unsigned char delayms) {
   analogWrite(beeppin, 20);
