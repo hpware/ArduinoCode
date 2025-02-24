5ee14dd70d0627da23d69a1843e04e4fd03bfbf8
flash_user <hw@yuanhau.com>
2025-02-24 10:45:25 +0800

    Update serial communication for devices and correct pin assignments

--- sketch_feb24a/sketch_feb24a.ino
+++ sketch_feb24a/sketch_feb24a.ino
@@ -1,14 +1,17 @@
-// DEVICE 2
-#define RX1 0
-#define TX1 2
+
+// DEVICE 2 (ESP32-WROOM-32D)
+#define RX2 16
+#define TX2 17
 
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600, SERIAL_8N1, RX1, TX1);
+  Serial2.begin(9600, SERIAL_8N1, RX2, TX2); // Using Serial2
+  Serial.println("Device 2 (ESP32-WROOM-32D) started.");
 }
+
 void loop() {
-  if (Serial1.available()) {
-    String receivedData = Serial1.readStringUntil('\n');
+  if (Serial2.available()) { // Using Serial2
+    String receivedData = Serial2.readStringUntil('\n');
     Serial.print("Received: ");
     Serial.println(receivedData);
   }
--- sketch_feb24b/sketch_feb24b.ino
+++ sketch_feb24b/sketch_feb24b.ino
@@ -1,11 +1,15 @@
-// DEVICE 1
+// DEVICE 1 (ESP32 Super Mini)
 #define RX1 0
 #define TX1 1
-
+      
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600, SERIAL_8N1, RX1, TX1);
+  Serial1.begin(9600, SERIAL_8N1, RX1, TX1); // Correct pins already
+  Serial.println("Device 1 (Super Mini) started.");
 }
+
 void loop() {
   Serial1.println("Net");
+  Serial.println("data");
+  delay(1000);
 }
